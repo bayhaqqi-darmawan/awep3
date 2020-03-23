f3d5865779c604267b6ae0ad77cc50ed05452cdb
Bayhaqqi <18ftt1707@student.pb.edu.bn>
2020-03-23 20:24:56 +0800

    commit

--- nav.php
+++ nav.php
@@ -2,10 +2,10 @@
     echo
     '
     <nav>
-        <div class="logo"><img src="logo.jpg" alt="GameLab" id="limg"></div>
+        <div class="logo"><a href="index.php"><img src="logo.jpg" alt="GameLab" id="limg"></a></div>
             <ul class="navlinks">
                 <li>
-                    <a href="#">Home</a>
+                    <a href="index.php">Home</a>
                 </li>
                 <li>
                     <a href="#">About</a>
