f5d8a3d1daad76e4d0d71cfc70ba9f56eb931caf
Bayhaqqi <18ftt1707@student.pb.edu.bn>
2020-03-23 19:26:01 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <title>GameLab</title>
+</head>
+<body>
+    <?php include 'nav.php'; ?> 
+    <div class="line">
+        <h2>highlights</h2>
+
+        <div class="borderline"></div>
+    </div>
+
+    <div class="slide-container">
+        <span id="slider-image-1"></span>
+        <span id="slider-image-2"></span>
+        <span id="slider-image-3"></span>
+
+        <div class="image-container">
+            <img src="https://games.mxdwn.com/wp-content/uploads/2018/10/tile_witcher3_load_tiny.png" alt="The Witcher 3" class="slider-image">
+            <img src="https://www.cyberpunk.net/build/images/social-thumbnail-en-ab9301da.jpg" alt="Cyberpunk 2077" class="slider-image">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ6nzrrtnVsX4b_nCXNrx1a-M3RXOXNwA0lH9KZKiaH3qCsyrsp" alt="Monster Hunter World" class="slider-image">
+        </div>
+        
+        <div class="button-container">
+            <a href="#slider-image-1" class="slider-button"></a>
+            <a href="#slider-image-2" class="slider-button"></a>
+            <a href="#slider-image-3" class="slider-button"></a>
+        </div>
+    </div>
+
+    
+        <h3>Recommended for you</h3>
+        <div class="borderline"></div>
+    
+
+    <div class="inside-box">
+        <div class="games">
+            <div class="game-details">
+                <img src="https://cdn.staticneo.com/ew/thumb/a/ab/The_Surge_2_titel.jpg/662px-The_Surge_2_titel.jpg" alt="The Surge 2" class="image-w-title">
+                <div class="details">The Surge 2</div>
+            </div>
+            <div class="game-details">
+                <img src="https://i.ytimg.com/vi/0WL0AnOsNY8/maxresdefault.jpg" alt="Dark Souls 3" class="image-w-title">
+                <div class="details">Dark Souls 3</div>
+            </div>
+            <div class="game-details">
+                <img src="https://steamcdn-a.akamaihd.net/steam/apps/817540/capsule_616x353.jpg?t=1559307304" alt="SpellForce 3: Soul Harvest" class="image-w-title">
+                <div class="details">SpellForce 3</div>
+            </div>
+            <div class="game-details">
+                <img src="https://cdn.player.one/sites/player.one/files/2020/02/12/borderlands-3.jpg" alt="Borderlands 3" class="image-w-title">
+                <div class="details">Borderlands 3</div>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,32 @@
+<?php 
+    echo
+    '
+    <nav>
+        <div class="logo"><img src="logo.jpg" alt="GameLab" id="limg"></div>
+            <ul class="navlinks">
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">About</a>
+                </li>
+                <li>
+                    <a href="#">Community</a>
+                </li>
+            </ul>
+
+            <ul class="navlinks">
+                <li>
+                    <a href="#">Sign in</a>
+                </li>
+                <li>
+                    <form class="example" action="action_page.php">
+                        <input type="text" placeholder="Search.." name="search">
+                        <button type="submit"><i class="fa fa-search"></i></button>
+                    </form>
+                </li>
+            </ul>
+    </nav>
+    '
+    
+?>
